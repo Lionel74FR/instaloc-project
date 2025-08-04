4814a1c6efec1a89fb5431961784e8a860ca75df
Lionel74FR <79314773+Lionel74FR@users.noreply.github.com>
2025-08-04 20:44:51 +0200

    fix: forçage de re-build Vercel

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,5 @@
 // lib/auth.ts
-import { getServerSession } from "next-auth"; //chemin strict
+import { getServerSession } from "next-auth"; // Mise à jour pour forcer Vercel à recompiler
 import { authOptions } from "@/app/api/auth/[...nextauth]/auth-options";
 
 export const auth = async () => {
